a4a57dddd124c51f5436a58ea9b808bfd859f949
Gustavo Ferreira <65983794+gustavofdeoliveira@users.noreply.github.com>
2024-04-27 09:42:22 -0300

    fix: smartcontracts

--- offchain/modules/face_comparator.py
+++
@@ -1,7 +0,0 @@
-import face_recognition
-
-
-class FaceComparator:
-    @staticmethod
-    def compare_faces(known_encodings, unknown_encoding):
-        return face_recognition.compare_faces(known_encodings, unknown_encoding)
